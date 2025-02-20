6a349413a2340f20cc9f72bcb0d111c8f5afa08d
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-02-20 03:53:07 +0200

    Create a basic html skeleton of the calculator

---
+++ script.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+const operator = "";
+let num1, num2;
+
+function operate(num1, num2, operator){
+    
+}
