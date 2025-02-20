fbcc6393a9bbab6e0e279c17d8a88eb5c5dbc577
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-02-20 04:19:18 +0200

    Add operate function

--- script.js
+++ script.js
@@ -14,9 +14,18 @@ function divide(a, b) {
     return a / b;
 }
 
-const operator = "";
+let operator = "";
 let num1, num2;
 
 function operate(num1, num2, operator){
-    
+    switch(operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+    }
 }
