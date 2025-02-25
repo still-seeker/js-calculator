4df736e57d4ce78864b20d0c69b091058bdb31db
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-02-25 23:38:43 +0200

    Add functionality to operator buttons(-*+/)

--- script.js
+++ script.js
@@ -12,33 +12,62 @@ function divide(a, b) {
     return parseFloat(a) / parseFloat(b);
 }
 
-/**operate function carries out the computation */
-const operate = function(leftNum, rightNum, operator){
-    let output;
-    switch(operator) {
-        case "+":
-            output = add(leftNum, rightNum);
-        case "-":
-            output = subtract(leftNum, rightNum);
-        case "*":
-            output = multiply(leftNum, rightNum);
-        case "/":
-            if (rightNum === 0) {
-                output = "Error"
-            }
-            output = divide(leftNum, rightNum);
-    }
-}
-
 //initializing the variables
 let initialDisplay = "";
 let leftNum = '';
 let rightNum = '';
 let operator = '';
+waitingForSecondOperand = false;
 
+/**operate function carries out the computation */
+function operate(){
+    if (leftNum === null || operate === null || initialDisplay === ''){
+        return;
+    }
+    let rightNum = parseFloat(initialDisplay);
+    let result;
+    switch(operator) {
+        case "+":
+            result = add(leftNum, rightNum);
+            break;
+        case "-":
+            result = subtract(leftNum, rightNum);
+            break;
+        case "x":
+            result = multiply(leftNum, rightNum);
+            break;
+        case "/":
+            if (rightNum === 0) {
+                display.value = "Error"
+            }
+            result = divide(leftNum, rightNum);
+            break;
+        default:
+            return;
+    }
+    result = result.toString();
+    display.value = result;
+    initialDisplay = display.value;
+}
+
+function setOperation(op){
+    //Do nothing in case there is no input yet
+    if (initialDisplay === ''){
+        return;
+    }
+    //calculate the result if an operation is already pending
+    if (leftNum !== '') {
+        operate()
+    }
+    //reset the input for the next number
+    leftNum = parseFloat(initialDisplay);
+    operator = op;
+    waitingForSecondOperand = true;
+    initialDisplay = '';
+}
 
 /**Selecting the display and setting the default value to 0*/
-const display = document.querySelector(".screen");
+const display = document.querySelector("#screen");
 display.value = '0';
 
 function appendToScreen(input){
@@ -46,10 +75,14 @@ function appendToScreen(input){
     display.value = initialDisplay;  
 }
 
+
 //Restores the input value to default
 function clearCalc() {
     display.value = '0';
     initialDisplay = '';
+    operator = '';
+    leftNum = '';
+    waitingForSecondOperand = false;
 }
 
 //deletes the last entered value from the screen
@@ -58,8 +91,18 @@ function del(){
     initialDisplay = display.value; 
 }
 
-//divides input by 100
+//divides result by 100
 function divideBy100() {
     display.value = parseFloat(display.value / 100);
     initialDisplay = display.value;
+}
+
+/**The function below handles the decimal point operations */
+function decimalPoint() {
+    if (!display.value.includes('.')){
+        appendToScreen('.')
+    }
+    else {
+        initialDisplay = display.value;
+    }
 }
